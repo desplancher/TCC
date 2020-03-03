09295811c19c09c04062b6166afae58f6e7693a9
desplancher <desplancher@users.noreply.github.com>
2020-03-02 22:51:15 -0300

    A esteira funcionou
    Com ajuda da Herim a esteira fumegou

---
+++ TCC/Assets/Movimento.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public float velocidade = 0.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        collisionInfo.transform.position += Vector3.right * velocidade;
+}
+}
